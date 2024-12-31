5dc135512c5bdbcc2d846a7cba72f594f594dcd6
Rich-Brandon <94618629+ricoace10@users.noreply.github.com>
2024-12-31 01:44:28 +0100

    index.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Layout from "../components/Layout";
+
+const Home = () => {
+  return (
+    <Layout>
+      <div className="text-center mt-20">
+        <h1 className="text-5xl font-bold text-yellow-500">TalkSport</h1>
+        <div className="bg-gray-200 w-4/5 h-60 mx-auto mt-8 rounded-lg shadow-md"></div>
+        <p className="text-yellow-500 text-lg mt-4 italic">Let’s Talk Football...</p>
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
