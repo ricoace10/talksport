dca70b6b5f6c4d82e3311892288227be5a97e014
Rich-Brandon <94618629+ricoace10@users.noreply.github.com>
2024-12-31 01:44:51 +0100

    register.tsx

---
+++ src/pages/register.tsx
@@ -0,0 +1,14 @@
+import Layout from "../components/Layout";
+
+const Register = () => {
+  return (
+    <Layout>
+      <div className="text-center mt-20">
+        <h1 className="text-3xl font-bold text-yellow-500">Register</h1>
+        <p className="text-gray-600 mt-4">Sign up to join TalkSport.</p>
+      </div>
+    </Layout>
+  );
+};
+
+export default Register;
