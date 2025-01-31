d792a4c5a250d6c9db1de32b58612f9956662415
Rich-Brandon <rico@constructionsourcing.se>
2025-01-31 16:48:29 +0100

    contrast error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ const Home = () => {
       <div className="text-center mt-20">
         <h1 className="text-5xl font-bold text-yellow-700">TalkSport</h1>
         <div className="bg-gray-400 w-4/5 h-60 mx-auto mt-8 rounded-lg shadow-md"></div>
-        <p className="text-yellow-700 text-lg mt-4 italic">Let’s Talk Football...</p>
+        <p className="text-yellow-500 text-lg mt-4 italic">Let’s Talk Football...</p>
       </div>
     </Layout>
   );
